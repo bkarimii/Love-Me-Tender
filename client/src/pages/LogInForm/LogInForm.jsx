4c9243c48df5888f9580cb734c4c2eb07294ffc2
karimi <behrouzkarimi1993@gmail.com>
2024-07-15 16:30:59 +0100

    creating logIn component

---
+++ client/src/pages/LogInForm/LogInForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LogIn(){
+
+
+    return(
+        <>
+            <form>
+                <label htmlFor="email">E-mail:</label>
+                <input type="email" id="email" name="email" />
+
+            </form>
+        </>
+    );
+}
+
+export default LogIn ;
