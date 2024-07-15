ccc5e583b44ac4d4b14a9930c98165cdab4dcb87
karimi <behrouzkarimi1993@gmail.com>
2024-07-15 17:46:31 +0100

    import login into APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import LogInForm from "./pages/LogInForm";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/login" element={<LogInForm />} />
 	</Routes>
 );
 
