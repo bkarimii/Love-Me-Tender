f158c8b7a85a5c8e529e571b114baaedc853c72a
karimi <behrouzkarimi1993@gmail.com>
2024-07-15 20:54:43 +0100

    first step , users select thier type by a radio button

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,26 +3,51 @@
 import React, { useState } from "react";
 
 function SignUp(){
-    const [userType , setUserType]=useState(null);
+    //to keep track value of radio buttons
+    const [userType , setUserType]=useState("");
+
+    //decides signup form to be shown or now
+    const [showForm , setShowForm]=useState(false);
     
+    const handleSubmitUserType=(e)=>{
+        e.preventDefault();
+        console.log(userType);
 
+    };
 
+    const handleChangeRdaio=(e)=>{
+        setUserType(e.target.value);
+    };
 
     return (
 			<>
 				<div>
 					<p>I am a :</p>
-					<form>
-						<div>
-							<input type="radio" name="user" value="contractor" />
-							<label htmlFor="contractor">Contractor</label>
-						</div>
-						<div>
-							<input type="radio" name="user" value="bidder" />
-							<label htmlFor="bidder">Bidder</label>
-						</div>
-						<button type="submit">Next</button>
-					</form>
+					{!showForm && (
+						<form onSubmit={handleSubmitUserType}>
+							<div>
+								<input
+									type="radio"
+									name="user"
+									value="contractor"
+									checked={userType === "contractor"}
+									onChange={handleChangeRdaio}
+								/>
+								<label htmlFor="contractor">Contractor</label>
+							</div>
+							<div>
+								<input
+									type="radio"
+									name="user"
+									value="bidder"
+									checked={userType === "bidder"}
+									onChange={handleChangeRdaio}
+								/>
+								<label htmlFor="bidder">Bidder</label>
+							</div>
+							<button type="submit">Next</button>
+						</form>
+					)}
 				</div>
 			</>
 		);
