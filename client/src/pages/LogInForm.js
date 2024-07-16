9414fa9223db18e6c617744c467af0c53ba1553b
karimi <behrouzkarimi1993@gmail.com>
2024-07-16 21:41:30 +0100

    bug and format fixing

--- client/src/pages/LogInForm.js
+++ client/src/pages/LogInForm.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 function LogInForm(){
     const [emailInput , setEmailInput]=useState("");
     const [passwordInput , setPasswordInput]=useState("") ;
+
+	//This Variable handle any error comes back from server to display it
+	const [backEndMessage , setBackEndMessage]=useState("");
+
     //Record changes for email address input
     const handleEmailChange = (e) => {
 			const email = e.target.value;
@@ -25,19 +29,21 @@ function LogInForm(){
 					},
 					body: JSON.stringify(userData),
 				});
-
+				const data=await response.json();
 				if (!response.ok) {
+					setBackEndMessage(data.message || "Something went wrong! Try again.");
 					throw new Error("Network response was not ok");
+				}else{
+					setBackEndMessage(data.message || "User signed up successfully!");
 				}
-
-				console.log("User signed up successfully!");
 			} catch (error) {
 				console.error("Error during signup:", error);
+				setBackEndMessage(error.message);
 			}
 		}
     const handleFormSubmit = (e) => {
 			e.preventDefault(); // Prevents the default form submission
-			const logInData={ emailInput , passwordInput };
+			const logInData={ email: emailInput ,password: passwordInput };
 
 
 			postLogInDeatils(logInData);
@@ -67,10 +73,13 @@ function LogInForm(){
 						/>
 						<button type="submit">Log In</button>
 					</form>
-                    <div><Link to={"/forgot-password"} >I forgot my password</Link></div>
 					<div>
+						<Link to={"/forgot-password"}>I forgot my password</Link>
+					</div>
+					<div>
+						{backEndMessage && <p>{backEndMessage}</p>}
 						<p>
-							Don&apos;t have an account? <Link to={"/signup"} >Sign Up </Link >
+							Don&apos;t have an account? <Link to={"/signup"}>Sign Up </Link>
 						</p>
 					</div>
 				</div>
