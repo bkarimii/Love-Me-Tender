f1e3d03fedff6fa30a3fb839c5c2debad43445cd
karimi <behrouzkarimi1993@gmail.com>
2024-07-19 01:34:41 +0100

    log-in session , and registeration endpoint added

--- server/api.js
+++ server/api.js
@@ -1,9 +1,96 @@
 import { Router } from "express";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
 router.get("/", (_, res) => {
 	res.status(200).json({ message: "WELCOME TO LOVE ME TENDER SITE" });
+});
+
+function validateEmail(email) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+}
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	const isEmailValid = validateEmail(email);
+	if (!isEmailValid) {
+		res.status(400).json({ message: "invalidEmailFormat" });
+	}
+	try {
+		const query = "SELECT * FROM users WHERE email = $1";
+		const dbResponse = await pool.query(query, [email]);
+		if (dbResponse.rows[0].length === 0) {
+			res.status(404).json({ message: "notRegistered" });
+		}
+
+		const storedHashedPassword = dbResponse.rows[0].password;
+		// Compare the password with the stored hashed password
+		const passwordMatches = await bcrypt.compare(
+			password,
+			storedHashedPassword
+		);
+		if (!passwordMatches) {
+			return res.status(401).json({
+				message: "worngPassword",
+			});
+		}
+		res.status(201).json({ message: "successful" });
+	} catch (error) {
+		res.status(500).json({ message: "serverError" });
+	}
+});
+
+router.post("/register-users", async (req, res) => {
+	const userType = req.body.userType;
+	const {
+		firstName,
+		lastName,
+		userName,
+		email,
+		password,
+		confirmPassword,
+		company,
+		description,
+		address,
+	} = req.body;
+
+	if (password !== confirmPassword) {
+		return res.status(400).json({ message: "mismatchPassword" });
+	}
+
+	try {
+		const queryToCheckEmail = "SELECT * FROM users WHERE email = $1";
+		const emailCheckResponse = await pool.query(queryToCheckEmail, [email]);
+
+		if (emailCheckResponse.rows.length > 0) {
+			return res.status(409).json({ message: "emailAlreadyExists" });
+		}
+		// Hash the password
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const queryToInsertUsers =
+			"INSERT INTO users(email, password_hash, userType ) VALUES($1, $2, $3) RETURNING id";
+		const insertIntoUsers = await pool.query(queryToInsertUsers, [
+			email,
+			hashedPassword,
+			userType,
+		]);
+		const userId = insertIntoUsers.rows[0].id;
+		if (userType === "bidder") {
+			const queryForBidder =
+				"INSERT INTO bidder (user_id, user_name, first_name, last_name, last_update) VALUES ($1, $2, $3, $4, NOW())";
+			await pool.query(queryForBidder, [userId, userName, firstName, lastName]);
+		} else if (userType === "buyer") {
+			const buyerQuery =
+				"INSERT INTO buyer (user_id, company, description, address, last_update) VALUES ($1, $2, $3, $4, NOW())";
+			await pool.query(buyerQuery, [userId, company, description, address]);
+		} else {
+			return res.status(400).json({ message: "invalidType" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "serverError" });
+	}
 });
 
 router.get("/skills", (req, res) => {
@@ -86,7 +173,7 @@ router.get("/skills", (req, res) => {
 });
 
 router.post("/publish-tenders", (req, res) => {
-    const formData = req.body;
+	const formData = req.body;
 
 	const newErrors = [];
 
