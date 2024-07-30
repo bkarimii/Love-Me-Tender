be3958d3ae42c21f19795ee1e712e58480ca0734
A Karimi <125777824+bkarimii@users.noreply.github.com>
2024-07-30 21:35:36 +0100

    Create log-in form (frontend) #7 -Behrouz (#30)
    * creating logIn component
    
    add Link for Login form in home.js
    
    import login into APP.js
    
    inital structure of the login form
    
    change login.jsx to login.js
    
    styling login link
    
    set email and password input . validate email address
    
    validate password and email for logIn
    
    adding forgot password
    
    code revised , redundant removed , fetch post added
    
    format fixedformatting error fixed
    
    fix prettier settings
    
    * requested changes applied
    
    * correction applied
    
    * correction applied
    
    * Update LogInForm.js
    
    * comment removed
    
    * save the token and minor fixes
    
    * fix desplay message on fetch response
    
    * corrections applied
    
    * correction applied , navigate hook fixed

---
+++ client/src/pages/LogInForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function LogInForm() {
+	const [emailInput, setEmailInput] = useState("");
+	const [passwordInput, setPasswordInput] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const navigate = useNavigate();
+
+	const handleEmailChange = (e) => {
+		const email = e.target.value;
+		setEmailInput(email);
+	};
+
+	const handlePasswordChange = (e) => {
+		const password = e.target.value;
+		setPasswordInput(password);
+	};
+
+	async function postLogInDeatils(userData) {
+		try {
+			const response = await fetch("/api/sign-in", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(userData),
+			});
+			const data = await response.json();
+			if (response.ok) {
+				const token = data.resources.token;
+				localStorage.setItem("authToken", token);
+				navigate("/admin-dashboard");
+			} else {
+				switch (response.status) {
+					case 401:
+						setErrorMessage("Incorrect password or email.");
+						break;
+					case 500:
+						setErrorMessage("Internal server error. Please try again later.");
+						break;
+					default:
+						setErrorMessage("An error occurred. Please try again.");
+				}
+			}
+		} catch (error) {
+			setErrorMessage("Unknown Error happened try again later.");
+		}
+	}
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		const logInData = { email: emailInput, password: passwordInput };
+		postLogInDeatils(logInData);
+	};
+
+	return (
+		<>
+			<div>
+				<form onSubmit={handleFormSubmit}>
+					<label htmlFor="email">E-mail:</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						value={emailInput}
+						onChange={handleEmailChange}
+						required
+					/>
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						value={passwordInput}
+						onChange={handlePasswordChange}
+						required
+					/>
+					<button type="submit">Log In</button>
+				</form>
+			</div>
+			<div>{errorMessage}</div>
+		</>
+	);
+}
+
+export default LogInForm;
