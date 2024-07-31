71f6353b715ad3934b7f5536e13062cd35398092
Aisha Athman <93427457+aishaathmanlali@users.noreply.github.com>
2024-07-31 17:45:11 +0100

    #61 - Build http client library - Aisha Athman Lali (#66)
    * build a client for adding auth header to requests
    
    * refactor code according to PR feedback
    
    * fix code

---
+++ client/src/TenderClient.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const getToken = () => localStorage.getItem("authToken");
+
+const client = axios.create({
+	baseURL: "http://localhost:3000",
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+client.interceptors.request.use((config) => {
+	const token = getToken();
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
+});
+
+const handleAuthError = (error) => {
+	if (error.response && error.response.status === 401) {
+		localStorage.removeItem("authToken");
+		window.location.href = "/sign-in";
+	} else {
+		throw error;
+	}
+};
+
+export const get = async (endpoint) => {
+	try {
+		const response = await client.get(endpoint);
+		return response.data;
+	} catch (error) {
+		handleAuthError(error);
+	}
+};
+
+export const post = async (endpoint, data) => {
+	try {
+		const response = await client.post(endpoint, data);
+		return response.data;
+	} catch (error) {
+		handleAuthError(error);
+	}
+};
+
+export const put = async (endpoint, data) => {
+	try {
+		const response = await client.put(endpoint, data);
+		return response.data;
+	} catch (error) {
+		handleAuthError(error);
+	}
+};
+
+export const del = async (endpoint) => {
+	try {
+		const response = await client.delete(endpoint);
+		return response.data;
+	} catch (error) {
+		handleAuthError(error);
+	}
+};
