73a666979afe341864e59963ce93f087c64220be
Adniya Yousaf <38715602+adniyaYousaf@users.noreply.github.com>
2024-08-01 19:41:52 +0100

    Customise the nav based on roles | Adniya (#82)
    * customise the nav based on roles
    
    * Applied the corrections
    
    * removed extra indentaions

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,27 +1,90 @@
 import Logo from "./assets/images/CTY-logo-rectangle.png";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Header.css";
 
 const Header = () => {
+	const [role, setRole] = useState(null);
+
+	useEffect(() => {
+		const userType = localStorage.getItem("userType");
+		setRole(userType);
+	}, []);
+
 	return (
 		<header className="header">
 			<img className="logo" src={Logo} alt="Code Your Future logo" />
 			<nav className="nav-list">
-				<li>
-					<NavLink exact to="/" className="nav-link" activeClassName="active">
-						Home{" "}
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						exact
-						to="/list-tenders"
-						className="nav-link"
-						activeClassName="active"
-					>
-						All Tenders{" "}
-					</NavLink>
-				</li>
+				{role === "admin" && (
+					<>
+						<NavLink
+							exact
+							to="/list-tenders"
+							className="nav-link"
+							activeClassName="active"
+						>
+							All Tenders
+						</NavLink>
+						<NavLink
+							exact
+							to="/signup"
+							className="nav-link"
+							activeClassName="active"
+						>
+							Grant Access
+						</NavLink>
+					</>
+				)}
+
+				{role === "buyer" && (
+					<>
+						<NavLink
+							exact
+							to="/list-tenders"
+							className="nav-link"
+							activeClassName="active"
+						>
+							All Tenders
+						</NavLink>
+						<NavLink
+							exact
+							to="/BuyerTenderList"
+							className="nav-link"
+							activeClassName="active"
+						>
+							My Tenders
+						</NavLink>
+						<NavLink
+							exact
+							to="/publish-tender"
+							className="nav-link"
+							activeClassName="active"
+						>
+							Publish Tenders
+						</NavLink>
+					</>
+				)}
+
+				{role === "bidder" && (
+					<>
+						<NavLink
+							exact
+							to="/list-tenders"
+							className="nav-link"
+							activeClassName="active"
+						>
+							All Tenders
+						</NavLink>
+						<NavLink
+							exact
+							to="/BidderBiddingList"
+							className="nav-link"
+							activeClassName="active"
+						>
+							My bids
+						</NavLink>
+					</>
+				)}
 			</nav>
 		</header>
 	);
