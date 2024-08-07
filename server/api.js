3d537bca7c98b994ef3e78ac3b0071d5e4d3c1b0
Aisha Athman <93427457+aishaathmanlali@users.noreply.github.com>
2024-08-07 13:08:29 +0100

    Add error logs to sign up and submit bid endpoints - Aisha Athman Lali (#132)
    * add console.error to find the bug
    
    * add console.error

--- server/api.js
+++ server/api.js
@@ -189,6 +189,7 @@ router.post("/signup", async (req, res) => {
 			userTableValues = [userId, company, description, address];
 		} else {
 			await client.query("ROLLBACK");
+			console.error(error); // eslint-disable-line no-console, no-undef
 			return res.status(500).json({ code: "SERVER_ERROR" });
 		}
 
@@ -213,6 +214,7 @@ router.post("/signup", async (req, res) => {
 		res.status(201).json({});
 	} catch (error) {
 		await client.query("ROLLBACK");
+		console.error(error); // eslint-disable-line no-console, no-undef
 		res.status(500).json({ code: "SERVER_ERROR" });
 	} finally {
 		client.release();
@@ -662,6 +664,7 @@ router.post("/bid", async (req, res) => {
 		const errors = [];
 
 		if (!validStatuses.includes(status)) {
+			console.error(error); // eslint-disable-line no-console, no-undef
 			res.status(500).json({ code: "SERVER_ERROR" });
 		}
 
@@ -728,11 +731,13 @@ router.post("/bid", async (req, res) => {
 			});
 		} catch (error) {
 			await client.query("ROLLBACK");
+			console.error(error); // eslint-disable-line no-console, no-undef
 			res.status(500).json({ code: "SERVER_ERROR" });
 		} finally {
 			client.release();
 		}
 	} catch (error) {
+		console.error(error); // eslint-disable-line no-console, no-undef
 		res.status(500).json({ code: "SERVER_ERROR" });
 	}
 });
