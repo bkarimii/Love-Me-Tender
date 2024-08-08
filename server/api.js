f7258afedfe74d46eeeda2f2652038300d3e3226
Adniya Yousaf <38715602+adniyaYousaf@users.noreply.github.com>
2024-08-08 16:26:00 +0100

    After publishing a tender, I got a message telling me it published successfully, but it doesn't appear in my tenders list | Adniya (#151)
    * fic the query
    
    * corections

--- server/api.js
+++ server/api.js
@@ -402,10 +402,10 @@ router.get("/tenders", async (req, res) => {
 
 	const countSql = "SELECT COUNT(*) FROM tender";
 	const tendersSql = `
-		SELECT id, title, creation_date, announcement_date, deadline, tender.description, status, closing_date, tender.last_update, buyer.company
-        FROM tender JOIN buyer ON tender.buyer_id = buyer.user_id
-        ORDER BY ${sort}  DESC
-		LIMIT $1 OFFSET $2
+		SELECT id, title, creation_date, announcement_date, deadline, description, status, closing_date, last_update
+        FROM tender
+        ORDER BY creation_date DESC
+		    LIMIT $1 OFFSET $2
 	`;
 
 	try {
