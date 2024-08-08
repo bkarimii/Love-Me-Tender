324c850921b735113f69e1a357c2ca40c43f0b46
Aisha Athman <93427457+aishaathmanlali@users.noreply.github.com>
2024-08-08 14:58:04 +0100

    Disable the withdraw button after bid withdrawal (#149)

--- client/src/BidderBiddingList.js
+++ client/src/BidderBiddingList.js
@@ -111,12 +111,14 @@ const BidderBiddingList = () => {
 									{bid.suggested_duration_days} days
 								</span>
 							</p>
-							<button
-								className="btn withdraw-btn"
-								onClick={() => handleStatusChange(bid.bid_id, "Withdrawn")}
-							>
-								Withdraw
-							</button>
+							{bid.status !== "Withdrawn" && (
+								<button
+									className="btn withdraw-btn"
+									onClick={() => handleStatusChange(bid.bid_id, "Withdrawn")}
+								>
+									Withdraw
+								</button>
+							)}
 						</div>
 					))
 				)}
